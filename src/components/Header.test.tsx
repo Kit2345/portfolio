1c7db78ad8a9958b2851590dec990cc35e41db41
Wing-Kit Leung <wing_kitleung@hotmail.com>
2025-01-22 14:13:45 +0000

    added header component and initial test

---
+++ src/components/Header.test.tsx
@@ -0,0 +1,10 @@
+import { screen, render } from "@testing-library/react";
+import Header from "./Header";
+describe("Header tests - start - will change later", () => {
+  it("should render the title", () => {
+    render(<Header />);
+    expect(screen.getByText("This is the header")).toHaveTextContent(
+      "This is the header"
+    );
+  });
+});
