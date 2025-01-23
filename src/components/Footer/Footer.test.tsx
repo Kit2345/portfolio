0d20ce70f571d4b2e487d38948a5cfe52aa41d19
Wing-Kit Leung <wing_kitleung@hotmail.com>
2025-01-23 15:20:29 +0000

    three buttons correctly labelled tests

--- src/components/Footer/Footer.test.tsx
+++ src/components/Footer/Footer.test.tsx
@@ -6,8 +6,11 @@ describe("Footer renders correctly", () => {
 
   it("Footer should exist", () => {});
 
-  it("should have three buttons", () => {
+  it("should have three buttons and correctly labelled", () => {
     const buttons = screen.getAllByRole("button");
     expect(buttons).toHaveLength(3);
+    expect(screen.getByText("About Me")).toBeInTheDocument;
+    expect(screen.getByText("Projects")).toBeInTheDocument;
+    expect(screen.getByText("Contact Me")).toBeInTheDocument;
   });
 });
