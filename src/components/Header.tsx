8a299ffe8e91528476240eef826bb4dabcaa7f05
Wing-Kit Leung <wing_kitleung@hotmail.com>
2025-01-23 13:45:30 +0000

    structure for header added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,14 @@
+import grass_energy from "../assets/grass_energy_card_vector_symbol_by_biochao_dezrx3b.png";
+
 function Header() {
-  return <header>This is the header</header>;
+  return (
+    <div>
+      <div>Full-stack developer</div>
+      <div>Wing-Kit Leung</div>
+      <button aria-label="menu">menu</button>
+      <img src={grass_energy} alt="Grass Energy" className="energy-symbol" />
+    </div>
+  );
 }
 
 export default Header;
