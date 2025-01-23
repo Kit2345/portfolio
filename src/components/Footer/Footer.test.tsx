f20b3336efbf3909651c1f218b488ea46e7d8176
Wing-Kit Leung <wing_kitleung@hotmail.com>
2025-01-23 15:13:17 +0000

    failing test for footer existing

---
+++ src/components/Footer/Footer.test.tsx
@@ -0,0 +1,7 @@
+import { Screen, render } from "@testing-library/react";
+
+describe("Footer renders correctly", () => {
+  beforeEach(() => render(<Footer />));
+
+  it("Footer should exist", () => {});
+});
