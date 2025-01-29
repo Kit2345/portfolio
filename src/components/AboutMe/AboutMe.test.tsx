6bdb844ba38d289517970bd95f81c53ad3a9ff1d
Wing-Kit Leung <wing_kitleung@hotmail.com>
2025-01-29 15:30:27 +0000

    About me test exisiting written and now passes

---
+++ src/components/AboutMe/AboutMe.test.tsx
@@ -0,0 +1,8 @@
+import { screen, render } from "@testing-library/react";
+import AboutMe from "./AboutMe";
+
+describe("About Me page should render properly", () => {
+  beforeEach(() => render(<AboutMe />));
+
+  it("should exist", () => {});
+});
