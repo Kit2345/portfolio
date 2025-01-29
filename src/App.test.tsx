29c2e62bba9a91b63444f649c16ced9b1fd4fce4
Wing-Kit Leung <wing_kitleung@hotmail.com>
2025-01-29 14:24:03 +0000

    moved footer and header to app from main

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,13 +2,7 @@ import { screen, render } from "@testing-library/react";
 import App from "./App";
 
 describe("App tests", () => {
-  it("should render the title", () => {
-    render(<App />);
-
-    expect(
-      screen.getByRole("heading", {
-        level: 1,
-      })
-    ).toHaveTextContent("Vite + React");
+  it("App renders properly", () => {
+    beforeEach(() => render(<App />));
   });
 });
