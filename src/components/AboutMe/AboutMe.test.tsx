2e18feebec71ea745264925f801e2c0ce1ff882e
Wing-Kit Leung <wing_kitleung@hotmail.com>
2025-01-29 17:10:56 +0000

    added tests for three images (bullet points), and three sections. Then updated about me to pass these tests

--- src/components/AboutMe/AboutMe.test.tsx
+++ src/components/AboutMe/AboutMe.test.tsx
@@ -6,14 +6,25 @@ describe("About Me page should render properly", () => {
 
   it("should exist", () => {});
 
-  it("should have an About Me blurb", () => {
-    const test = screen.getByText("About Me");
-    expect(test).toBeInTheDocument();
+  it("should have an h1 heading, About Me blurb,", () => {
+    const aboutMe = screen.getByText("About Me");
+    expect(aboutMe).toBeInTheDocument();
+
+    const heading = screen.getByRole("heading", { level: 1 });
+    expect(heading).toBeInTheDocument;
   });
 
   it("should have an image", () => {
-    const test = screen.getByRole("img");
-    expect(test).toBeInTheDocument();
+    const test = screen.getAllByRole("img");
+    expect(test[0]).toBeInTheDocument();
+  });
+
+  it("should have three images and three sub headings (h3)", () => {
+    const test_img = screen.getAllByRole("img");
+    expect(test_img).toHaveLength(3);
+
+    const test_heading = screen.getAllByRole("heading", { level: 3 });
+    expect(test_heading).toHaveLength(3);
   });
 
   it("this test should fail", () => {
