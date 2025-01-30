47afab1600a1a65540828e21443b88c6504ff3bf
Wing-Kit Leung <wing_kitleung@hotmail.com>
2025-01-30 13:11:05 +0000

    Contact me page now exists and passes test

---
+++ src/components/ContactMe/ContactMe.test.tsx
@@ -0,0 +1,8 @@
+import { screen, render } from "@testing-library/react";
+import ContactMe from "./ContactMe";
+
+describe("contact me page should render correctly", () => {
+  beforeEach(() => render(<ContactMe />));
+
+  it("Contact Me page should exist", () => {});
+});
